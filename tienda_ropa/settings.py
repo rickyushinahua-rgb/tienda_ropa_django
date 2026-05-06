ad9d229f5028321376264fe53ec4e68db9aa699d
Ricky Marty <ricky.ushinahua@tecsup.edu.pe>
2026-05-06 12:13:26 -0500

    Agregar dominio Railway a CSRF trusted origins

--- tienda_ropa/settings.py
+++ tienda_ropa/settings.py
@@ -30,6 +30,9 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://web-production-851f7.up.railway.app',
+]
 
 # Application definition
 
